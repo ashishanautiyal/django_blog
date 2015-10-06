7390e343737834b69f535fc65715798a5dd108aa
ashishanautiyal <nautiyalashisha@gmail.com>
2015-10-06 12:03:16 +0530

    added new branch for python problems

---
+++ pythonChalanges/abstractclass.py
@@ -0,0 +1,42 @@
+"""
+1 Create an abstract base class called "Human".Give
+  it an abstract function called "run"
+
+2. Create an abstract base class called "Robot". Give it
+    an abstract function called vacuum
+
+3. Create a subclass which inherits from both "Human"
+   and "Robots" called cyborg and create an instance of cyborg
+
+"""
+
+
+from abc import ABCMeta, abstractmethod
+
+class Human(object):
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def run(self):
+        pass
+
+class Robot(object):
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def vacum(self):
+        pass
+
+
+class Cyborg(Human, Robot):
+
+    def run(self):
+        print('cyborg is running')
+
+    def vacum(self):
+        print('cyborg is in vacum')
+
+x = Cyborg()
+x.run()
+x.vacum()
+
